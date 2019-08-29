2a6ec71c794e1fc3a9426814e9beb61045a7fa9c
marianofreddi <marianofreddi@gmail.com>
2019-08-29 14:51:54 -0300

    add sketch article, home

---
+++ drafts_html/maf.js
@@ -0,0 +1,8 @@
+// toggle css classes 
+var article_table_config = document.getElementsByClassName('config'), i;
+
+function hide() {
+    for (var i = 0; i < article_table_config.length; i ++) {
+        article_table_config[i].classList.toggle('collapse');
+    }
+};
