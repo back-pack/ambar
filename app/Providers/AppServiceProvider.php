21242a513b7764390a98bed4cb4edcb5c9fde967
Santiago Graviano <santiago.graviano@gmail.com>
2019-08-30 15:51:42 -0300

    Blade form components added.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::include('components.form.bootstrap.input', 'input');
+        Blade::include('components.form.bootstrap.custom.select', 'select');
+        Blade::include('components.form.bootstrap.custom.checkbox', 'checkbox');
+        Blade::include('components.form.bootstrap.checkbox-boolean', 'checkbox_boolean');
+        Blade::include('components.form.bootstrap.textarea', 'textarea');
+        Blade::include('components.form.bootstrap.field-errors', 'field_errors');
     }
 }
