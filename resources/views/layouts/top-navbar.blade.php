33c330f33436389a2acaef384fff5207b87a582f
marianofreddi <marianofreddi@gmail.com>
2019-09-08 20:58:49 -0300

    fixed top-narbar.blade

--- resources/views/layouts/top-navbar.blade.php
+++ resources/views/layouts/top-navbar.blade.php
@@ -10,7 +10,7 @@
                 <a class="nav-link" href="#">Pedidos</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Articulos</a>
+                <a class="nav-link" href="{{ route('articles.index') }}">Articulos</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
@@ -26,30 +26,3 @@
     </div>
 </nav>
 
-
-
-
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
-
-        <a href="/" class="navbar-brand">Ambar💕</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-navbar-menu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="top-navbar-menu">
-            <ul class="navbar-nav mr-auto">
-
-                <li class="nav-item dropdown">
-                    <a href="" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">Articulos</a>
-                    <div class="dropdown-menu">
-                        <a href="{{ route('articles.index') }}" class="dropdown-item">Ver todos</a>
-                        <a href="{{ route('articles.create') }}" class="dropdown-item">Ingresar nuevo</a>
-                    </div>
-                </li>
-
-            </ul>
-        </div>
-
-    </div>
-</nav>
