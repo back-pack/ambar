7986634e8e5743d9cde711077c5a90685f49565e
marianofreddi <marianofreddi@gmail.com>
2019-09-08 21:23:04 -0300

    index.blade delete alert("me hago caca")

--- resources/views/model/article/index.blade.php
+++ resources/views/model/article/index.blade.php
@@ -8,7 +8,7 @@
 
 
     <div class="col-sm">
-      <p class='h2'>Articulos | <a href="{{ route('articles.create') }}" ><button onclick="alert('agrega articulo')" class="btn btn-sm btn-outline-success" type="button"><i class="fas fa-plus-square"></i></button></a> <button onclick="alert('Imprime lista')" class="btn btn-sm btn-outline-secondary" type="button"><i class="fas fa-print"></i></button> <button onclick="hide()" class="btn btn-sm btn-outline-primary" type="button"><i class="fas fa-tools"></i></button></p>
+      <p class='h2'>Articulos | <a href="{{ route('articles.create') }}" ><button class="btn btn-sm btn-outline-success" type="button"><i class="fas fa-plus-square"></i></button></a> <button onclick="alert('Imprime lista')" class="btn btn-sm btn-outline-secondary" type="button"><i class="fas fa-print"></i></button> <button onclick="hide()" class="btn btn-sm btn-outline-primary" type="button"><i class="fas fa-tools"></i></button></p>
       <table class="table table-hover table-responsive-sm">
         <thead class="thead-light">
           <tr>
