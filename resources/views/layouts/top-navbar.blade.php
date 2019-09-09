1ac640515d6e09ad959165af44af94cde8bf73fa
marianofreddi <marianofreddi@gmail.com>
2019-09-08 21:15:14 -0300

    fixed top-narbar.blade 2

--- resources/views/layouts/top-navbar.blade.php
+++ resources/views/layouts/top-navbar.blade.php
@@ -1,28 +1,30 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">Ambar💕</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pedidos</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="{{ route('articles.index') }}">Articulos</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-            <a class="dropdown-item" href="#">Markup</a>
-            <a class="dropdown-item" href="#">Clientes</a>
-            <a class="dropdown-item" href="#">Usuarios</a>
-            </div>
-            </li>
-        </ul>
-        <spam class="navbar-text nav-link">Hola $USER</spam>
-        <button type="button" class="btn btn-outline-secondary" alt="salir"><i class="fas fa-sign-out-alt"></i></button>
+        <a href="/" class="navbar-brand">Ambar💕</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-navbar-menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="top-navbar-menu">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pedidos</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('articles.index') }}">Articulos</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">Markup</a>
+                <a class="dropdown-item" href="#">Clientes</a>
+                <a class="dropdown-item" href="#">Usuarios</a>
+                </div>
+                </li>
+            </ul>
+            <spam class="navbar-text nav-link">Hola $USER</spam>
+            <button type="button" class="btn btn-outline-secondary" alt="salir"><i class="fas fa-sign-out-alt"></i></button>
+        </div>
     </div>
 </nav>
-
