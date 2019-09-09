5a2c419c6ef1c4fd1f663498115eab4eced0aca2
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-09 17:18:12 -0300

    Price field changed to cost. Weight field added. Modal display added when article attempt to be deleted.

--- resources/views/model/article/create.blade.php
+++ resources/views/model/article/create.blade.php
@@ -1,25 +1,46 @@
 @extends('layouts.master')
 
 @section('title')
-    <title>Crear articulo</title>
+    <title>Crear artículo</title>
 @endsection
 
 @section('content')
 
+    <h2>Crear artículo</h2>
+    <hr>
+
     <form method="post" action="{{ route('articles.store') }}">
       @csrf
 
-      @input([
-        'name' => 'name',
-        'label' => 'Nombre',
-        'attributes' => ['type' => 'text']
-      ])
+      <div class="form-row">
 
-      @input([
-        'name' => 'price',
-        'label' => 'Precio',
-        'attributes' => ['type' => 'number', 'step' => 'any']
-      ])
+        <div class="col-md-4">
+          @input([
+            'name' => 'name',
+            'label' => 'Nombre',
+            'attributes' => ['type' => 'text']
+          ])
+        </div>
+
+        <div class="col-md-4">
+          @input([
+            'name' => 'cost',
+            'label' => 'Costo',
+            'input_group_prepend_text' => ['$'],
+            'attributes' => ['type' => 'number', 'step' => 'any']
+          ])
+        </div>
+
+        <div class="col-md-4">
+          @input([
+            'name' => 'weight',
+            'label' => 'Peso',
+            'input_group_append_text' => ['kg'],
+            'attributes' => ['type' => 'number', 'step' => 'any']
+          ])
+        </div>
+
+      </div>
 
       @input([
         'name' => 'description',
--- resources/views/model/article/edit.blade.php
+++ resources/views/model/article/edit.blade.php
@@ -1,28 +1,50 @@
 @extends('layouts.master')
 
 @section('title')
-    <title>Editar articulo</title>
+    <title>Editar artículo</title>
 @endsection
 
 @section('content')
+
+    <h2>Editar artículo</h2>
+    <hr>
 
     <form method="post" action="{{ route('articles.update', ['id' => $article->id]) }}">
       @csrf
       @method('patch')
 
-      @input([
-        'name' => 'name',
-        'label' => 'Nombre',
-        'attributes' => ['type' => 'text'],
-        'value' => $article->name
-      ])
+      <div class="form-row">
 
-      @input([
-        'name' => 'price',
-        'label' => 'Precio',
-        'attributes' => ['type' => 'number', 'step' => 'any'],
-        'value' => $article->price
-      ])
+        <div class="col-md-4">
+          @input([
+            'name' => 'name',
+            'label' => 'Nombre',
+            'attributes' => ['type' => 'text'],
+            'value' => $article->name
+          ])
+        </div>
+
+        <div class="col-md-4">
+          @input([
+            'name' => 'cost',
+            'label' => 'Costo',
+            'input_group_prepend_text' => ['$'],
+            'attributes' => ['type' => 'number', 'step' => 'any'],
+            'value' => $article->cost
+          ])
+        </div>
+
+        <div class="col-md-4">
+          @input([
+            'name' => 'weight',
+            'label' => 'Peso',
+            'input_group_append_text' => ['kg'],
+            'attributes' => ['type' => 'number', 'step' => 'any'],
+            'value' => $article->weight
+          ])
+        </div>
+
+      </div>
 
       @input([
         'name' => 'description',
@@ -31,7 +53,26 @@
         'value' => $article->description
       ])
 
+      @include('components.form.bootstrap.form-errors')
+
       <button type="submit" class="btn btn-primary">Editar</button>
     </form>
+
+    <hr>
+
+    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#destroy_article_modal">Eliminar</button>
+
+    @component('components.modal', ['id' => 'destroy_article_modal', 'title' => 'Eliminar artículo'])
+        ¿Desea eliminar el artículo {{ $article->name }}?
+        @slot('footer')
+          <button class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <form method="post" action="{{ route('articles.destroy', ['id' => $article->id]) }}">
+            @csrf
+            @method('delete')
+
+            <button type="submit" name="button" class="btn btn-danger">Eliminar</button>
+          </form>
+        @endslot
+    @endcomponent
 
 @endsection
