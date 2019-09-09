fd4cb4aba82765c0e37bfa710900b75dd15910d2
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-09 17:05:44 -0300

    Table responsiveness added. Fields and footer added.

--- resources/views/model/article/index.blade.php
+++ resources/views/model/article/index.blade.php
@@ -6,29 +6,36 @@
 
 @section('content')
 
-    <table class="table table-hover table-responsive-sm">
-      <thead class="thead-light">
-        <tr>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Descripción</th>
-          <th>Precio</th>
-          <th>Ultima actualización</th>
-          <th>Acción</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($articles as $article)
+    <h2>Artículos</h2>
+    <hr>
+
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead class="thead-light">
           <tr>
-            <td>{{ $article->id }}</td>
-            <td>{{ $article->name }}</td>
-            <td>{{ $article->description }}</td>
-            <td>{{ $article->price_formated }}</td>
-            <td>{{ $article->price_last_update->format('d/m/Y') }}</td>
-            <td><a href="{{ route('articles.edit', ['id' => $article->id]) }}">Editar</a></td>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Peso</th>
+            <th>Costo</th>
+            <th>Ultima actualización</th>
+            <th>Acción</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @foreach ($articles as $article)
+            <tr>
+              <td>{{ $article->id }}</td>
+              <td>{{ $article->name }}</td>
+              <td>{{ $article->description }}</td>
+              <td>{{ $article->weight }}kg</td>
+              <td>{{ $article->cost_formated }}</td>
+              <td>{{ $article->cost_last_update->format('d/m/Y') }}</td>
+              <td><a href="{{ route('articles.edit', ['id' => $article->id]) }}">Editar</a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
 
 @endsection
