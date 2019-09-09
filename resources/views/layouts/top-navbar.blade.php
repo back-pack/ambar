6058a11fdd3f02cea86f8b8c8ab89ff1f94b1889
marianofreddi <marianofreddi@gmail.com>
2019-09-08 22:47:23 -0300

    add maf.js

--- resources/views/layouts/top-navbar.blade.php
+++ resources/views/layouts/top-navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
+    <div class="container-fluid">
 
         <a href="/" class="navbar-brand">Ambar💕</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top-navbar-menu">
