2b3d3d8792077ff3cb67807c51d4f15a409fd730
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-12 14:12:55 -0300

    Input prepend and append functionality added.

--- resources/views/components/form/bootstrap/input.blade.php
+++ resources/views/components/form/bootstrap/input.blade.php
@@ -13,21 +13,37 @@
 @else
   <div class="form-group @isset($classes['wrapper']) {{ $classes['wrapper'] }} @endisset">
     <label for="{{ $name }}_input" class="@isset($classes['label']) {{ $classes['label'] }} @endisset">{{ $label }}</label>
-    <input
-      class="form-control @isset($classes['input']) {{ $classes['input'] }} @endisset @error($name) is-invalid @enderror"
-      name="{{ $name }}"
-      id="{{ $name }}_input"
-      @if (isset($value) && !old($name))
-        value="{{ $value }}"
-      @else
-        value="{{ old($name) }}"
-      @endif
-      @isset($attributes)
-        @foreach ($attributes as $key => $value)
-            {{ $key }}="{{ $value }}"
-        @endforeach
+    <div class="input-group">
+      @isset($input_group_prepend_text)
+        <div class="input-group-prepend">
+          @foreach ($input_group_prepend_text as $text)
+              <div class="input-group-text">{{ $text }}</div>
+          @endforeach
+        </div>
       @endisset
-    >
+      <input
+        class="form-control @isset($classes['input']) {{ $classes['input'] }} @endisset @error($name) is-invalid @enderror"
+        name="{{ $name }}"
+        id="{{ $name }}_input"
+        @if (isset($value) && !old($name))
+          value="{{ $value }}"
+        @else
+          value="{{ old($name) }}"
+        @endif
+        @isset($attributes)
+          @foreach ($attributes as $key => $value)
+              {{ $key }}="{{ $value }}"
+          @endforeach
+        @endisset
+      >
+      @isset($input_group_append_text)
+        <div class="input-group-append">
+          @foreach ($input_group_append_text as $text)
+              <div class="input-group-text">{{ $text }}</div>
+          @endforeach
+        </div>
+      @endisset
+    </div>
     @field_errors(['name' => $name])
   </div>
 @endif
