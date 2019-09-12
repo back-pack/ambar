e532558668ccc62bb7df70aef00be16c9717830e
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-12 14:13:41 -0300

    Now form errors show all errors.

--- resources/views/components/form/bootstrap/form-errors.blade.php
+++ resources/views/components/form/bootstrap/form-errors.blade.php
@@ -1,8 +1,8 @@
-{{-- @if ($errors->any())
+@if ($errors->any())
     @foreach ($errors->all() as $error)
       <div class="alert alert-danger">{{ $error }}</div>
     @endforeach
-@endif --}}
-@if ($errors->has('exists'))
-    <div class="alert alert-info">{{ $errors->first('exists') }} Puedes editarlo <a href="{{ route('reports.edit', ['id' => session('exists_id')]) }}">aqui</a></div>
 @endif
+{{-- @if ($errors->has('exists'))
+    <div class="alert alert-info">{{ $errors->first('exists') }} Puedes editarlo <a href="{{ route('reports.edit', ['id' => session('exists_id')]) }}">aqui</a></div>
+@endif --}}
