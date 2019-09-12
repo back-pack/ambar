648c7e422b2efed6e3f50c2fc387e794964f983f
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-12 14:11:06 -0300

    ArticlesTableSeeder included. Seeders registeered in DatabaseSeeder.

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $articles = [
+            ['name' => 'Granola 3kg', 'description' => 'Granola de la buena.', 'cost' => 30.75, 'cost_last_update' => now(), 'weight' => 3],
+            ['name' => 'Granola 5kg', 'description' => 'Granola de la buena.', 'cost' => 45, 'cost_last_update' => now(), 'weight' => 5],
+            ['name' => 'Arvejas 1kg', 'description' => 'Arvejas de las buenas.', 'cost' => 10.75, 'cost_last_update' => now(), 'weight' => 1],
+            ['name' => 'Maiz inflado 0.5kg', 'description' => 'Maiz inflado del bueno.', 'cost' => 5, 'cost_last_update' => now(), 'weight' => 0.5],
+            ['name' => 'Maiz inflado 1kg', 'description' => 'Maiz inflado del bueno.', 'cost' => 8.5, 'cost_last_update' => now(), 'weight' => 1],
+        ];
+
+        foreach ($articles as $article) {
+            \DB::table('articles')->insert($article);
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(MarginsTableSeeder::class);
+        $this->call(ClientsTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
     }
 }
