4c470201bd6097609bb22363e29bd3f06c8d3565
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-13 15:03:42 -0300

    Delete button added. Index design improved.

--- resources/views/model/order/edit.blade.php
+++ resources/views/model/order/edit.blade.php
@@ -5,4 +5,21 @@
 
     <edit-order order_id="{{ $order->id }}"></edit-order>
 
+    <hr>
+
+    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#destroy_order_modal">Eliminar</button>
+
+    @component('components.modal', ['id' => 'destroy_order_modal', 'title' => 'Eliminar pedido'])
+        ¿Desea eliminar el pedido {{ $order->name }}?
+        @slot('footer')
+          <button class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <form method="post" action="{{ route('orders.destroy', ['id' => $order->id]) }}">
+            @csrf
+            @method('delete')
+
+            <button type="submit" name="button" class="btn btn-danger">Eliminar</button>
+          </form>
+        @endslot
+    @endcomponent
+
 @endsection
--- resources/views/model/order/index.blade.php
+++ resources/views/model/order/index.blade.php
@@ -1,8 +1,39 @@
 @extends('layouts.master')
 
+@section('title')
+    <title>Pedidos</title>
+@endsection
 
 @section('content')
 
+
+    <div class="col-sm">
+      <p class='h2'>Pedidos | <a href="{{ route('orders.create') }}" ><button class="btn btn-sm btn-outline-success" type="button"><i class="fas fa-plus-square"></i></button></a> <button onclick="alert('Imprime lista')" class="btn btn-sm btn-outline-secondary" type="button"><i class="fas fa-print"></i></button> <button onclick="hide()" class="btn btn-sm btn-outline-primary" type="button"><i class="fas fa-tools"></i></button></p>
+      <table class="table table-hover table-responsive-sm">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Cliente</th>
+            <th scope="col">Entrega</th>
+            <th scope="col">Artículos</th>
+            <th scope="col">Total</th>
+            <th class="config" scope="col"><i class="fas fa-tools config"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($orders as $order)
+            <tr>
+              <td scope="row">{{ $order->id }}</td>
+              <td>{{ $order->client->name }}</td>
+              <td>{{ $order->delivery_formatted }}</td>
+              <td>{{ $order->articles()->count() }}</td>
+              <td>{{ number_readable($order->total, "$") }}</td>
+              <td class='config'><a href="{{ route('orders.edit', ['id' => $order->id]) }}"><button type="button" class="btn btn-sm btn-outline-warning config"><i class="fas fa-wrench"></i></button></a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
 
 
 @endsection
