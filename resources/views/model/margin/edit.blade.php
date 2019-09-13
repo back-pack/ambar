41cb22c73fa1f01a7451470df673acb04ec05403
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-13 15:03:31 -0300

    Delete button added. Index design improved.

--- resources/views/model/margin/edit.blade.php
+++ resources/views/model/margin/edit.blade.php
@@ -20,12 +20,30 @@
         @input([
             'name' => 'profit',
             'label' => 'Ganancia',
-            'attributes' => ['type' => 'number', 'step' => 'any'],
+            'input_group_append_text' => ['%'],
+            'attributes' => ['type' => 'number', 'step' => 'any', 'min' => 0],
             'value' => $margin->profit
         ])
 
         <button type="submit" class="btn btn-primary">Editar margen</button>
 
     </form>
+
+    <hr>
+
+    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#destroy_margin_modal">Eliminar</button>
+
+    @component('components.modal', ['id' => 'destroy_margin_modal', 'title' => 'Eliminar mergen'])
+        ¿Desea eliminar el margen {{ $margin->name }}?
+        @slot('footer')
+          <button class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <form method="post" action="{{ route('margins.destroy', ['id' => $margin->id]) }}">
+            @csrf
+            @method('delete')
+
+            <button type="submit" name="button" class="btn btn-danger">Eliminar</button>
+          </form>
+        @endslot
+    @endcomponent
 
 @endsection
--- resources/views/model/margin/index.blade.php
+++ resources/views/model/margin/index.blade.php
@@ -5,20 +5,25 @@
 @endsection
 
 @section('content')
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Ganancia</th>
-            </tr>
+    <div class="col-sm">
+      <p class='h2'>Margenes | <a href="{{ route('margins.create') }}" ><button class="btn btn-sm btn-outline-success" type="button"><i class="fas fa-plus-square"></i></button></a> <button onclick="alert('Imprime lista')" class="btn btn-sm btn-outline-secondary" type="button"><i class="fas fa-print"></i></button> <button onclick="hide()" class="btn btn-sm btn-outline-primary" type="button"><i class="fas fa-tools"></i></button></p>
+      <table class="table table-hover table-responsive-sm">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Ganancia</th>
+            <th class="config" scope="col"><i class="fas fa-tools config"></i></th>
+          </tr>
         </thead>
         <tbody>
-            @foreach ($margins as $margin)
-                <tr>
-                    <td>{{ $margin->name }}</td>
-                    <td>{{ $margin->profit }}</td>
-                </tr>
-            @endforeach
+          @foreach ($margins as $margin)
+            <tr>
+              <td scope="row">{{ $margin->name }}</td>
+              <td>{{ number_readable($margin->profit, null, "%", true) }}</td>
+              <td class='config'><a href="{{ route('margins.edit', ['id' => $margin->id]) }}"><button type="button" class="btn btn-sm btn-outline-warning config"><i class="fas fa-wrench"></i></button></a></td>
+            </tr>
+          @endforeach
         </tbody>
-    </table>
+      </table>
+    </div>
 @endsection
