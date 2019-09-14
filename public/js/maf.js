c405eb3c2d2fe5a72c8ea21b0b64ced4929f39b8
Oscar <oscar.eber@gmail.com>
2019-09-14 12:10:39 -0300

    logica para mostrar modal

--- public/js/maf.js
+++ public/js/maf.js
@@ -10,3 +10,18 @@ function hide() {
 function date_hide() {
 	$('#datee').toggle('hide');
 }
+
+var delete_button = document.getElementsByClassName('trash_delete');
+for(var x = 0; x < delete_button.length; x++) {
+    delete_button[x].addEventListener("click", function(e) {
+        e.preventDefault();
+        var $id = this.id;
+        var $entity = this.dataset.entity;
+        alert($entity);
+        $modal = document.getElementById('modal');
+        $modal.style.display = 'block';
+})};
+
+function closeModal(element) {
+    element.style.display = 'none';
+}
