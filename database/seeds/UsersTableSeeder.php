82b7b0a5bbef993fe0db25cc6b054927aa342832
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-17 16:28:03 -0300

    Non admin user added. Api tokens added.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,8 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $users = [
-            ['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => Hash::make('Pass.001'), 'phone' => '12345678', 'is_admin' => true, 'is_active' => true]
+            ['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => Hash::make('Pass.001'), 'api_token' => \Str::random(60), 'phone' => '12345678', 'is_admin' => true, 'is_active' => true],
+            ['name' => 'Vendedor', 'email' => 'vendedor@ambar.com', 'password' => Hash::make('Pass.002'), 'api_token' => \Str::random(60), 'phone' => '12345678', 'is_admin' => false, 'is_active' => true]
         ];
 
         foreach ($users as $user) {
