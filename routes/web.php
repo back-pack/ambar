1b9eb159a94ed10e39eb7634337059a710f6de69
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-20 23:22:19 -0300

    Article PDF generation route added.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('articles/pdf', 'ArticleController@pdf')->name('articles.pdf');
+
 Route::resource('clients', 'ClientController');
 Route::resource('orders', 'OrderController');
 Route::resource('articles', 'ArticleController');
