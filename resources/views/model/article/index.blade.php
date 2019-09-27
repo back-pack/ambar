9095e0a4a810c5e06b2007c461d71939d0d8a453
Santiago Graviano <santiago.graviano@gmail.com>
2019-09-27 14:14:56 -0300

    Articles Search component added.

--- resources/views/model/article/index.blade.php
+++ resources/views/model/article/index.blade.php
@@ -9,6 +9,7 @@
 
     <div class="col-sm">
       <p class='h2'>Articulos | <a href="{{ route('articles.create') }}" ><button class="btn btn-sm btn-outline-success" type="button"><i class="fas fa-plus-square"></i></button></a> <a href="{{ route('articles.pdf') }}"><button class="btn btn-sm btn-outline-secondary" type="button"><i class="fas fa-print"></i></button></a> <button onclick="hide()" class="btn btn-sm btn-outline-primary" type="button"><i class="fas fa-tools"></i></button></p>
+      <articles-search></articles-search>
       <table class="table table-hover table-responsive-sm">
         <thead class="thead-light">
           <tr>
