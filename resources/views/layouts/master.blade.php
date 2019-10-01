ac62ad285e5e0a64060757f45ae1fc121e6b9689
Santiago Graviano <santiago.graviano@gmail.com>
2019-10-01 13:21:03 -0300

    CSS Doodle added. maf.js moved. Blade Stack "js" added.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,16 +13,20 @@
 </head>
 <body>
   <div id="app">
-      @include('layouts.top-navbar')
+      @if (auth()->check())
+          @include('layouts.top-navbar')
+      @endif
       <div class="container-fluid">
         @yield('content')
       </div>
   </div>
-  <script src="/js/maf.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/css-doodle/0.5.1/css-doodle.min.js"></script>
   <script src="https://kit.fontawesome.com/bf90177acd.js"></script>
+  <script src="/js/maf.js"></script>
   <script src="{{ mix('js/app.js') }}"></script>
   {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> --}}
+    @stack('js')
 </body>
 </html>
