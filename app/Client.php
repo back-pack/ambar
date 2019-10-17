d5b579c34fec11338b26ccb3503da87acb77705c
Santiago Graviano <santiago.graviano@gmail.com>
2019-10-17 00:33:41 -0300

    payments relationship added. toSelectOption function added. getDebtAttribute and getHasDebtAttribute added.

--- app/Client.php
+++ app/Client.php
@@ -16,4 +16,27 @@ class Client extends Model
     {
         return $this->hasMany('App\Order');
     }
+
+    public function payments()
+    {
+        return $this->hasMany('App\Payment');
+    }
+
+    public function toSelectOption()
+    {
+        return [$this->id, $this->name];
+    }
+
+    public function getDebtAttribute()
+    {
+        $orders_total = $this->orders->sum('total');
+        $payments_total = $this->payments->sum('amount');
+
+        return $orders_total - $payments_total;
+    }
+
+    public function getHasDebtAttribute()
+    {
+        return $this->debt > 0;
+    }
 }
