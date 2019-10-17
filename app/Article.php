d8eb3386a63137de28358d8a0654c6720c1d133d
Santiago Graviano <santiago.graviano@gmail.com>
2019-10-17 00:29:11 -0300

    category_id, price and price_last_update fields added. margin and margin_last_update field removed. Filter by Category added. getPriceAttribute commented out.

--- app/Article.php
+++ app/Article.php
@@ -3,17 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pipeline\Pipeline;
 
 class Article extends Model
 {
-    protected $fillable = ['name', 'description', 'cost', 'cost_last_update', 'margin', 'margin_last_update', 'weight'];
+    protected $fillable = ['category_id', 'name', 'description', 'cost', 'cost_last_update', 'price', 'price_last_update', 'weight'];
 
-    protected $dates = ['cost_last_update', 'margin_last_update'];
+    protected $dates = ['cost_last_update', 'price_last_update'];
 
     protected $casts = [
         'cost' => 'decimal:2',
         'weight' => 'decimal:2'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(\App\Category::class);
+    }
+
+    public static function filtered()
+    {
+        return app(Pipeline::class)
+            ->send(Article::query())
+            ->through([
+                \App\QueryFilters\Article\Category::class
+            ])
+            ->thenReturn();
+    }
 
     /**
      * Accessors & Mutators
@@ -24,14 +40,14 @@ class Article extends Model
         return '$' . number_format($this->attributes['cost'], 2, ',', '.');
     }
 
-    public function getPriceAttribute()
-    {
-        $price = (float) $this->cost + $this->margin;
-        // $x = 5;
-        //
-        // return (ceil($price) % $x === 0) ? ceil($price) : round(($price + $x / 2) / $x) * $x;
-
-        return $price;
-    }
+    // public function getPriceAttribute()
+    // {
+    //     $price = (float) $this->cost + $this->margin;
+    //     // $x = 5;
+    //     //
+    //     // return (ceil($price) % $x === 0) ? ceil($price) : round(($price + $x / 2) / $x) * $x;
+    //
+    //     return $price;
+    // }
 
 }
